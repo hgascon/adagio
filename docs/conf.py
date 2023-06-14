ccf52a2a4f7e7d6676f940355c78f30e5aa1fc4e
Hugo Gascón <hgascon@users.noreply.github.com>
2023-06-14 16:05:02 +0200

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -29,7 +29,7 @@ import os
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'sphinx.ext.pngmath',
+    'sphinx.ext.imgmath',
     'sphinx.ext.viewcode',
 ]
 
